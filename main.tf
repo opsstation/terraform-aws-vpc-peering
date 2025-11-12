7a7f40f3d5760052ff4044c64666d0eb50c13b83
Roshanyadav18 <roshan.opsstation@gmail.com>
2025-11-12 17:05:11 +0530

    update

--- main.tf
+++ main.tf
@@ -52,10 +52,10 @@ resource "aws_vpc_peering_connection" "default" {
 resource "aws_vpc_peering_connection" "region" {
   count = var.enable_peering && !var.auto_accept ? 1 : 0
 
-  vpc_id      = var.requestor_vpc_id
-  peer_vpc_id = var.acceptor_vpc_id
-  auto_accept = false
-  peer_region = local.accept_region
+  vpc_id        = var.requestor_vpc_id
+  peer_vpc_id   = var.acceptor_vpc_id
+  auto_accept   = false
+  peer_region   = local.accept_region
   peer_owner_id = var.peer_owner_id != "" ? var.peer_owner_id : null
 
   tags = merge(
